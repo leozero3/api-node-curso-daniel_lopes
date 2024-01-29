4dc81eb8995f1009842b0c341075e62029eee1e4
Leo Home <realleo2009@hotmail.com>
2024-01-28 22:26:23 -0300

    aula 82, migration create post

---
+++ readme.txt
@@ -0,0 +1,3 @@
+Yarn sequelize-cli migration:generate --name inserirNomeAqui   //cria uma migration
+
+Yarn sequelize db:migrate  // iniciar nova tabela no DB
---
+++ src/database/migrations/20240129005205-create-posts.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("posts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      number_likes: {
+        type: Sequelize.STRING,
+        defaultValue: 0,
+      },
+      author_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("posts");
+  },
+};
